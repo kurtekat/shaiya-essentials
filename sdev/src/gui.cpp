a39f26b5f2a9bf5446b4cb2699eb8a734939ae0d
Spelunkern <150851205+Spelunkern@users.noreply.github.com>
2024-08-03 12:12:14 -0300

    Update gui.cpp

--- sdev/src/gui.cpp
+++ sdev/src/gui.cpp
@@ -169,4 +169,5 @@ void hook::gui()
     // ignore exp multiplication
     // note: does not affect locale 0 or 1 (different math)
     util::write_memory((void*)0x4FA494, 0x44, 1);
+    util::write_memory((void*)0x4F9852, 0x00, 1);
 }
